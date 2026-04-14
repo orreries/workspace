36a2619bae135bdce13b2e7b733fe9a24a36f2e1
Oriel Mwaniki-Alexander <hmwaniki@pratt.edu>
2026-04-14 17:56:22 -0400

    adding database stuff

--- pratt-info-638-2026-main/playing_with_sql.sql
+++ pratt-info-638-2026-main/playing_with_sql.sql
@@ -2,8 +2,7 @@ drop database if exists my_first_db;
 
 create database my_first_db;
 
-\c my_first_db;
-
+\c my_first_db
 
 create table authors (
   id serial primary key,
@@ -24,18 +23,21 @@ CREATE TABLE books (
 );
 
 
+insert into authors (first_name, last_name) values ('william', 'shakespeare');
+insert into authors (first_name, last_name) values ('jane', 'austen');
 insert into authors (first_name, last_name) values ('leo', 'tolstoy');
+insert into authors (first_name, last_name) values ('virginia', 'woolf');
 insert into authors (first_name, last_name) values ('James', 'S. A. Corey');
 insert into authors (first_name, last_name) values ('Craig', 'Alanson');
 insert into authors (first_name, last_name) values ('Cixin', 'Liu');
 insert into authors (first_name, last_name) values ('John', 'Scalzi');
 
-insert into bookrs (title, publishing_year) values ('The Bluest Eye', '1960');
-insert into bookrs (title, publishing_year) values ('pride and prejudice', '1800');
-insert into bookrs (title, publishing_year) values ('king lear', '1700');
-insert into bookrs (title, publishing_year) values ('the giving tree', '1960')
+insert into genres (name) values ('Sience Fiction');
+insert into genres (name) values ('Fantasy');
+insert into genres (name) values ('Romance');
+insert into genres (name) values ('Mystery');
+insert into genres (name) values ('Horror');
 
-insert into genres (title, publishing_year) values ('Horror', '0000')
-insert into genres (title, publishing_year) values ('SciFi', '0000')
-insert into genres (title, publishing_year) values ('Thriller', '0000')
 
+insert into books (title, publishing_year, genre_id) values ('Leviathan Wakes', 2011, (select id from genres where name = 'Sience Fiction'));
+insert into books (title, publishing_year, genre_id) values ('Caliban’s War', 2012, (select id from genres where name = 'Sience Fiction'));
